ae0988e2253cbe463a36583a0b4e9dba52ea0a46
pakmatthew <matthewtamkp@gmail.com>
2022-07-26 18:04:05 +0800

    update keyboard input

--- src/FKeyboardInputView/index.tsx
+++ src/FKeyboardInputView/index.tsx
@@ -1,21 +1,16 @@
 import React from "react";
-import { Platform } from "react-native";
+import { KeyboardAvoidingView } from "react-native";
 import { FKeyboardInputViewProps } from "./types";
-import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 export const FKeyboardInputView = (props: FKeyboardInputViewProps) => {
 	return (
-		<KeyboardAwareScrollView
-			enableOnAndroid={props.enableOnAndroid ?? true}
+		<KeyboardAvoidingView
+			behavior={props.behavior ?? "padding"}
 			style={[{ flex: 1 }, props.style]}
 			contentContainerStyle={[{ flexGrow: 1 }, props.contentContainerStyle]}
-			showsVerticalScrollIndicator={props.showsVerticalScrollIndicator ?? false}
-			extraHeight={props.extraHeight ?? Platform.OS === "ios" ? 0 : 75}
-			extraScrollHeight={
-				props.extraScrollHeight ?? Platform.OS === "ios" ? 80 : 180
-			}
-			keyboardShouldPersistTaps={props.keyboardShouldPersistTaps ?? "handled"}
-			children={props.children}
-		/>
+			{...props}
+		>
+			{props.children}
+		</KeyboardAvoidingView>
 	);
 };
